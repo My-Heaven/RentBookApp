d510dcd4b574635bd8026ad3c635754fc3c74e96
vuha-soon <hanhvu0978@gmail.com>
2021-03-24 19:53:02 +0700

    ctor

--- RentBookApp/DTO/BookDTO.cs
+++ RentBookApp/DTO/BookDTO.cs
@@ -18,5 +18,16 @@ namespace RentBookApp.DTO
         public string  createDate { get; set; }
         public bool status { get; set; }
 
+        public BookDTO(string bookTitle, int quantity, float price, int typeID, string author, int publishingYear, string createDate, bool status)
+        {
+            this.bookTitle = bookTitle;
+            this.quantity = quantity;
+            this.price = price;
+            this.typeID = typeID;
+            this.author = author;
+            this.publishingYear = publishingYear;
+            this.createDate = createDate;
+            this.status = status;
+        }
     }
 }
